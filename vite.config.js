b824862f13bd91e428ec941ac54a6522ca7b5217
François <76422000+Francois-lqc@users.noreply.github.com>
2024-11-06 17:45:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    // base: "/brain\_vrar",
+    base: "/brain_vrar",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
