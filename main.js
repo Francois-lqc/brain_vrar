11818911bf4c02b6680202def5df95727fdf135b
pauline.marsaudon <pauline.marsaudon@epita.fr>
2024-11-30 16:50:01 +0100

    add sound

--- main.js
+++ main.js
@@ -4,24 +4,27 @@
 // see https://developer.mozilla.org/en-US/docs/Glossary/Tree_shaking)
 
 import {
-  AmbientLight,
-  BoxGeometry,
-  Clock,
-  Color,
-  CylinderGeometry,
-  Group,
-  HemisphereLight,
-  LoadingManager,
-  Mesh,
-  MeshBasicMaterial,
-  MeshNormalMaterial,
-  MeshPhongMaterial,
-  PerspectiveCamera,
-  Raycaster,
-  RingGeometry,
-  Scene,
-  Vector3,
-  WebGLRenderer
+    AmbientLight,
+    Audio,
+    AudioListener,
+    AudioLoader,
+    BoxGeometry,
+    Clock,
+    Color,
+    CylinderGeometry,
+    Group,
+    HemisphereLight,
+    LoadingManager,
+    Mesh,
+    MeshBasicMaterial,
+    MeshNormalMaterial,
+    MeshPhongMaterial,
+    PerspectiveCamera,
+    Raycaster,
+    RingGeometry,
+    Scene,
+    Vector3,
+    WebGLRenderer
 } from 'three';
 
 // XR Emulator
@@ -51,11 +54,11 @@ import { XRButton } from 'three/addons/webxr/XRButton.js';
 // 
 // Consider using alternatives like Oimo or cannon-es
 import {
-  OrbitControls
+    OrbitControls
 } from 'three/addons/controls/OrbitControls.js';
 
 import {
-  GLTFLoader
+    GLTFLoader
 } from 'three/addons/loaders/GLTFLoader.js';
 
 // Example of hard link to official repo for data, if needed
@@ -68,36 +71,36 @@ let objectScaled = [];
 
 async function setupXR(xrMode) {
 
-  if (xrMode !== 'immersive-vr') return;
+    if (xrMode !== 'immersive-vr') return;
 
-  // iwer setup: emulate vr session
-  let nativeWebXRSupport = false;
-  if (navigator.xr) {
-    nativeWebXRSupport = await navigator.xr.isSessionSupported(xrMode);
-  }
+    // iwer setup: emulate vr session
+    let nativeWebXRSupport = false;
+    if (navigator.xr) {
+        nativeWebXRSupport = await navigator.xr.isSessionSupported(xrMode);
+    }
 
-  if (!nativeWebXRSupport) {
-    const xrDevice = new XRDevice(metaQuest3);
-    xrDevice.installRuntime();
-    xrDevice.fovy = (75 / 180) * Math.PI;
-    xrDevice.ipd = 0;
-    window.xrdevice = xrDevice;
-    xrDevice.controllers.right.position.set(0.15649, 1.43474, -0.38368);
-    xrDevice.controllers.right.quaternion.set(
-      0.14766305685043335,
-      0.02471366710960865,
-      -0.0037767395842820406,
-      0.9887216687202454,
-    );
-    xrDevice.controllers.left.position.set(-0.15649, 1.43474, -0.38368);
-    xrDevice.controllers.left.quaternion.set(
-      0.14766305685043335,
-      0.02471366710960865,
-      -0.0037767395842820406,
-      0.9887216687202454,
-    );
-    new DevUI(xrDevice);
-  }
+    if (!nativeWebXRSupport) {
+        const xrDevice = new XRDevice(metaQuest3);
+        xrDevice.installRuntime();
+        xrDevice.fovy = (75 / 180) * Math.PI;
+        xrDevice.ipd = 0;
+        window.xrdevice = xrDevice;
+        xrDevice.controllers.right.position.set(0.15649, 1.43474, -0.38368);
+        xrDevice.controllers.right.quaternion.set(
+            0.14766305685043335,
+            0.02471366710960865,
+            -0.0037767395842820406,
+            0.9887216687202454,
+        );
+        xrDevice.controllers.left.position.set(-0.15649, 1.43474, -0.38368);
+        xrDevice.controllers.left.quaternion.set(
+            0.14766305685043335,
+            0.02471366710960865,
+            -0.0037767395842820406,
+            0.9887216687202454,
+        );
+        new DevUI(xrDevice);
+    }
 }
 
 await setupXR('immersive-ar');
@@ -108,151 +111,161 @@ let controller, group, raycaster;
 // Main loop
 const animate = (timestamp, frame) => {
 
-  if (frame) {
-    const referenceSpace = renderer.xr.getReferenceSpace();
-    const session = renderer.xr.getSession();
+    if (frame) {
+        const referenceSpace = renderer.xr.getReferenceSpace();
+        const session = renderer.xr.getSession();
 
-    if (hitTestSourceRequested === false) {
-      session.requestReferenceSpace('viewer').then(function (referenceSpace) {
-        session.requestHitTestSource({ space: referenceSpace }).then(function (source) {
-          hitTestSource = source;
-        });
-      });
-      session.addEventListener('end', function () {
-        hitTestSourceRequested = false;
-        hitTestSource = null;
-      });
-      hitTestSourceRequested = true;
+        if (hitTestSourceRequested === false) {
+            session.requestReferenceSpace('viewer').then(function (referenceSpace) {
+                session.requestHitTestSource({ space: referenceSpace }).then(function (source) {
+                    hitTestSource = source;
+                });
+            });
+            session.addEventListener('end', function () {
+                hitTestSourceRequested = false;
+                hitTestSource = null;
+            });
+            hitTestSourceRequested = true;
 
+        }
+
+        if (hitTestSource) {
+            const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+            if (hitTestResults.length) {
+                const hit = hitTestResults[0];
+                reticle.visible = true;
+                reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
+
+            } else {
+                reticle.visible = false;
+            }
+        }
     }
-
-    if (hitTestSource) {
-      const hitTestResults = frame.getHitTestResults(hitTestSource);
-
-      if (hitTestResults.length) {
-        const hit = hitTestResults[0];
-        reticle.visible = true;
-        reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
-
-      } else {
-        reticle.visible = false;
-      }
-    }
-  }
-  // can be used in shaders: uniforms.u_time.value = elapsed;
-  renderer.render(scene, camera);
+    // can be used in shaders: uniforms.u_time.value = elapsed;
+    renderer.render(scene, camera);
 };
 
 
 const init = () => {
-  let isCerveauSpawn = false;
-  scene = new Scene();
+    let isCerveauSpawn = false;
+    scene = new Scene();
 
-  const aspect = window.innerWidth / window.innerHeight;
-  camera = new PerspectiveCamera(75, aspect, 0.1, 10); // meters
-  camera.position.set(0, 1.6, 3);
+    const aspect = window.innerWidth / window.innerHeight;
+    camera = new PerspectiveCamera(75, aspect, 0.1, 10); // meters
+    camera.position.set(0, 1.6, 3);
 
-  const light = new AmbientLight(0xffffff, 1.0); // soft white light
-  scene.add(light);
+    const light = new AmbientLight(0xffffff, 1.0); // soft white light
+    scene.add(light);
 
-  const hemiLight = new HemisphereLight(0xffffff, 0xbbbbff, 3);
-  hemiLight.position.set(0.5, 1, 0.25);
-  scene.add(hemiLight);
+    const hemiLight = new HemisphereLight(0xffffff, 0xbbbbff, 3);
+    hemiLight.position.set(0.5, 1, 0.25);
+    scene.add(hemiLight);
 
-  renderer = new WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animate); // requestAnimationFrame() replacement, compatible with XR 
-  renderer.xr.enabled = true;
-  document.body.appendChild(renderer.domElement);
+    renderer = new WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate); // requestAnimationFrame() replacement, compatible with XR 
+    renderer.xr.enabled = true;
+    document.body.appendChild(renderer.domElement);
 
-  const xrButton = XRButton.createButton(renderer, {});
-  xrButton.style.backgroundColor = 'blue';
-  document.body.appendChild(xrButton);
+    const xrButton = XRButton.createButton(renderer, {});
+    xrButton.style.backgroundColor = 'blue';
+    document.body.appendChild(xrButton);
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  //controls.listenToKeyEvents(window); // optional
-  controls.target.set(0, 1.6, 0);
-  controls.update();
+    var audioLoader = new AudioLoader();
+    var listener = new AudioListener();
+    var backgroundAudio = new Audio(listener);
+    audioLoader.load("assets/audios/background_sound.mp3", function (buffer) {
+        backgroundAudio.setBuffer(buffer);
+        backgroundAudio.setLoop(true);
+        backgroundAudio.setVolume(1);
+        backgroundAudio.play();
+    });
 
-  // Handle input: see THREE.js webxr_ar_cones
+    const controls = new OrbitControls(camera, renderer.domElement);
+    //controls.listenToKeyEvents(window); // optional
+    controls.target.set(0, 1.6, 0);
+    controls.update();
 
-  let manager = new LoadingManager();
+    // Handle input: see THREE.js webxr_ar_cones
 
-  manager.onProgress = function (url) {
-    if (url == 'big_brain.glb') {
-      console.log('Loading ' + url);
+    let manager = new LoadingManager();
+
+    manager.onProgress = function (url) {
+        if (url == 'big_brain.glb') {
+            console.log('Loading ' + url);
+        }
+    };
+
+    let loader = new GLTFLoader(manager).setPath('assets/models/')
+
+    const onSelect = (event) => {
+
+        if (reticle.visible) {
+
+            if (isCerveauSpawn == false) {
+                loader.load('big_brain.glb', gltfReader);
+                isCerveauSpawn = true;
+            }
+            let targetObject = null;
+            const controller = event.target;
+
+            // fonction qui me récupère tous les objets sur lesquels j'ai cliqué
+            const intersections = getIntersections(controller);
+
+            console.log(intersections);
+            if (intersections.length > 0) {
+                // si j'ai cliqué sur une liste d'objets, faire l'action que je veux
+
+                const intersection = intersections[0];
+
+                const object = intersection.object;
+                object.material.emissive.b = 1;
+
+                controller.userData.selected = object;
+                targetObject = object.parent;
+                console.log('J\'ai intercepté un object ' + targetObject.name);
+            }
+            if (targetObject) {
+                // add clicked object to the scaled objects array
+                objectScaled.push(targetObject);
+                // Compute camera position
+
+                // brain_rotate = false;
+
+                // Make object 10% bigger
+                targetObject.scale.set(targetObject.scale.x + 0.1, targetObject.scale.y + 0.1, targetObject.scale.z + 0.1)
+            }
+            else {
+                // brain_rotate = true;
+                // To revert back scaled object
+                objectScaled.forEach(object => { object.scale.set(object.scale.x - 0.1, object.scale.y - 0.1, object.scale.z - 0.1); });
+                objectScaled = [];
+            }
+            controller.userData.targetRayMode = event.data.targetRayMode;
+
+        }
     }
-  };
 
-  let loader = new GLTFLoader(manager).setPath('assets/models/')
+    controller = renderer.xr.getController(0);
+    controller.addEventListener('select', onSelect);
+    // controller.addEventListener('select', onSelectObject);
+    scene.add(controller);
+    group = new Group();
+    scene.add(group);
 
-  const onSelect = (event) => {
+    reticle = new Mesh(
+        new RingGeometry(0.15, 0.2, 32).rotateX(- Math.PI / 2),
+        new MeshBasicMaterial()
+    );
+    reticle.matrixAutoUpdate = false;
+    reticle.visible = false;
+    scene.add(reticle);
 
-    if (reticle.visible) {
+    raycaster = new Raycaster()
 
-      if (isCerveauSpawn == false) {
-        loader.load('big_brain.glb', gltfReader);
-        isCerveauSpawn = true;
-      }
-      let targetObject = null;
-      const controller = event.target;
-
-      // fonction qui me récupère tous les objets sur lesquels j'ai cliqué
-      const intersections = getIntersections(controller);
-
-      console.log(intersections);
-      if (intersections.length > 0) {
-        // si j'ai cliqué sur une liste d'objets, faire l'action que je veux
-
-        const intersection = intersections[0];
-
-        const object = intersection.object;
-        object.material.emissive.b = 1;
-
-        controller.userData.selected = object;
-        targetObject = object.parent;
-        console.log('J\'ai intercepté un object ' + targetObject.name);
-      }
-      if (targetObject) {
-        // add clicked object to the scaled objects array
-        objectScaled.push(targetObject);
-        // Compute camera position
-
-        // brain_rotate = false;
-
-        // Make object 10% bigger
-        targetObject.scale.set(targetObject.scale.x + 0.1, targetObject.scale.y + 0.1, targetObject.scale.z + 0.1)
-      }
-      else {
-        // brain_rotate = true;
-        // To revert back scaled object
-        objectScaled.forEach(object => { object.scale.set(object.scale.x - 0.1, object.scale.y - 0.1, object.scale.z - 0.1); });
-        objectScaled = [];
-      }
-      controller.userData.targetRayMode = event.data.targetRayMode;
-
-    }
-  }
-
-  controller = renderer.xr.getController(0);
-  controller.addEventListener('select', onSelect);
-  // controller.addEventListener('select', onSelectObject);
-  scene.add(controller);
-  group = new Group();
-  scene.add(group);
-
-  reticle = new Mesh(
-    new RingGeometry(0.15, 0.2, 32).rotateX(- Math.PI / 2),
-    new MeshBasicMaterial()
-  );
-  reticle.matrixAutoUpdate = false;
-  reticle.visible = false;
-  scene.add(reticle);
-
-  raycaster = new Raycaster()
-
-  window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('resize', onWindowResize, false);
 
 }
 
@@ -260,33 +273,33 @@ init();
 
 let brain_obj;
 function gltfReader(gltf) {
-  brain_obj = gltf.scene;
+    brain_obj = gltf.scene;
 
-  if (brain_obj != null) {
-    console.log("Model loaded:  " + brain_obj);
-    brain_obj.scale.set(0.1, 0.1, 0.1);
-    reticle.matrix.decompose(brain_obj.position, brain_obj.quaternion, brain_obj.scale)
-    group.add(brain_obj);
-    console.log(brain_obj);
-  } else {
-    console.log("Load FAILED.  ");
-  }
+    if (brain_obj != null) {
+        console.log("Model loaded:  " + brain_obj);
+        brain_obj.scale.set(0.1, 0.1, 0.1);
+        reticle.matrix.decompose(brain_obj.position, brain_obj.quaternion, brain_obj.scale)
+        group.add(brain_obj);
+        console.log(brain_obj);
+    } else {
+        console.log("Load FAILED.  ");
+    }
 
 }
 
 function getIntersections(controller) {
 
-  controller.updateMatrixWorld();
+    controller.updateMatrixWorld();
 
-  raycaster.setFromXRController(controller);
+    raycaster.setFromXRController(controller);
 
-  return raycaster.intersectObjects(group.children, true);
+    return raycaster.intersectObjects(group.children, true);
 
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
